a4ff295071b254f0008d3806627ab1e21732bf3f
clolmaugh <76975877+clolmaugh@users.noreply.github.com>
2021-04-24 14:36:51 -0700

    intro pg add

---
+++ drughistory/script.js
@@ -0,0 +1,43 @@
+class AnimatedFilter {
+    constructor(filterId, elementId) {
+      this.index = 0;
+      this.filterId = filterId;
+      this.element = document.getElementById(elementId);
+      this.isHover = false;
+      this.element.addEventListener('mouseenter', () => { this.isHover = true; });
+      this.element.addEventListener('mouseleave', () => { this.isHover = false; });
+  
+      this.update();
+    }
+  
+    get currentFilterId() {
+      return `${ this.filterId }-${ this.index }`;
+    }
+  
+    update() {
+      const frame = window.requestAnimationFrame(_ => this.update());
+      if(frame % 3 != 0) return;
+  
+      const sec = performance.now() / 1000;
+      
+      const eFilter = document.getElementById(this.currentFilterId);
+      
+      this.index = (this.index + 1) % 2;
+      eFilter.setAttribute('id', this.currentFilterId);
+  
+      const eNoise = eFilter.querySelector('feTurbulence');
+      const eDisplace = eFilter.querySelector(`feDisplacementMap`);
+  
+      const s1 = (sec % 3) * 0.001 + (Math.tan(Math.tan(sec)) * .5 + .5) * 0.001;
+      eNoise.setAttribute('baseFrequency', `0 ${ s1 * ((this.isHover) ? 1 : 0) }`);
+      const rnd = parseInt(Math.random() * 100);
+  
+      eNoise.setAttribute('seed', `${ ( Math.random() < 0.6 ) ? rnd : 0 }`);
+      
+      this.element.setAttribute('style', `filter: url(#${ this.currentFilterId });`);
+    }
+  };
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    const filter = new AnimatedFilter('glitch', 'hdg-1');
+  });
