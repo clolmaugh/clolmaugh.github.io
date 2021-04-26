520f78ae7bd741315b14e2d320b3baafee9cb3ad
clolmaugh <76975877+clolmaugh@users.noreply.github.com>
2021-04-25 17:18:39 -0700

    jquery add

--- drughistory/script.js
+++ drughistory/script.js
@@ -41,3 +41,29 @@ class AnimatedFilter {
   window.addEventListener('DOMContentLoaded', () => {
     const filter = new AnimatedFilter('glitch', 'hdg-1');
   });
+
+// Scroll Fadein
+
+$(document).ready(function() {
+  
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+});
+
+
+});
+
+
+
