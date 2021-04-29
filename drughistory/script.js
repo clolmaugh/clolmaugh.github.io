ac01e7eb4a8bb5890c62a1761f9e731db6ebfc92
clolmaugh <76975877+clolmaugh@users.noreply.github.com>
2021-04-28 21:41:42 -0700

    audio & link fixes

--- drughistory/script.js
+++ drughistory/script.js
@@ -1,69 +1,64 @@
-class AnimatedFilter {
-    constructor(filterId, elementId) {
-      this.index = 0;
-      this.filterId = filterId;
-      this.element = document.getElementById(elementId);
-      this.isHover = false;
-      this.element.addEventListener('mouseenter', () => { this.isHover = true; });
-      this.element.addEventListener('mouseleave', () => { this.isHover = false; });
-  
-      this.update();
-    }
-  
-    get currentFilterId() {
-      return `${ this.filterId }-${ this.index }`;
-    }
-  
-    update() {
-      const frame = window.requestAnimationFrame(_ => this.update());
-      if(frame % 3 != 0) return;
-  
-      const sec = performance.now() / 1000;
-      
-      const eFilter = document.getElementById(this.currentFilterId);
-      
-      this.index = (this.index + 1) % 2;
-      eFilter.setAttribute('id', this.currentFilterId);
-  
-      const eNoise = eFilter.querySelector('feTurbulence');
-      const eDisplace = eFilter.querySelector(`feDisplacementMap`);
-  
-      const s1 = (sec % 3) * 0.001 + (Math.tan(Math.tan(sec)) * .5 + .5) * 0.001;
-      eNoise.setAttribute('baseFrequency', `0 ${ s1 * ((this.isHover) ? 1 : 0) }`);
-      const rnd = parseInt(Math.random() * 100);
-  
-      eNoise.setAttribute('seed', `${ ( Math.random() < 0.6 ) ? rnd : 0 }`);
-      
-      this.element.setAttribute('style', `filter: url(#${ this.currentFilterId });`);
-    }
-  };
-  
-  window.addEventListener('DOMContentLoaded', () => {
-    const filter = new AnimatedFilter('glitch', 'hdg-1');
-  });
+
+ var audioPlaying = false,
+ audio
+
+ (function() {
+ audio = document.getElementById('audio');
+
+ var audioControl = document.getElementById('audio-control');
+ audioControl.addEventListener('click', audioControlHandler, false)
+}())
+
+function audioControlHandler(e) {
+ var _self = e.target;
+
+ if (!audioPlaying) {
+   _self.classList.add('noVolume')
+   audio.volume = 0
+ } else {
+   _self.classList.remove('noVolume')
+   audio.volume = 1
+ }
+ audioPlaying = !audioPlaying
+}
+
 
 // Scroll Fadein
 
 $(document).ready(function() {
-  
-$(document).on("scroll", function() {
-  var pageTop = $(document).scrollTop();
-  var pageBottom = pageTop + $(window).height();
-  var tags = $(".tag");
+    
+  $(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
 
-  for (var i = 0; i < tags.length; i++) {
-    var tag = tags[i];
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
 
-    if ($(tag).position().top < pageBottom) {
-      $(tag).addClass("visible");
-    } else {
-      $(tag).removeClass("visible");
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
     }
+  });
+
+// Turns background audio on and off  
+$("#audio-control").click(function () {
+  if ($("#background")[0].volume != 0) {
+    $("#background")[0].volume = 0
+  } else {
+    $("#background")[0].volume = 1
   }
 });
 
 
+// the little function that changes the source and loads it.
+function changeSource(sourceUrl) {
+  $("#fart-sound").attr("src", sourceUrl)
+  /****************/
+  player[0].pause()
+  player[0].load() // this is important since we're changing the source on the fly, we need to also load it
+}
+
 });
-
-
-
